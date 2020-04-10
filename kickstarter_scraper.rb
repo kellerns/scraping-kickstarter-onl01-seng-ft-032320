712b8b5ad887bffa43fa302a66034a30b5b4cac1
Nicholas Keller <nic.keller328@gmail.com>
2020-04-10 22:58:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,6 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
 
-  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
@@ -24,9 +23,5 @@ def create_project_hash
     }
   end
 
-  # return the projects hash
   projects
-
-
-  binding.pry
 end
